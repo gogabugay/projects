27135c650347d394b0248077466d591a5d556667
Yuri <yuriyroudenko@gmail.com>
2024-03-21 12:11:09 +0300

    Added a loop and save_game method. Started the load_game method

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 #getting a dictionary
 dict = File.readlines('google-10000-english-no-swears.txt')
 
@@ -10,7 +12,7 @@ while word.length < 5 || word.length > 12
 end
 
 #setting an array from the word
-word_arr = word.split("")
+p word_arr = word.split("")
 
 #method to get player's input
 def player_turn
@@ -31,9 +33,13 @@ right_letters = []
 def guess_in_word(guess, word, wrong_letters, right_letters)
   if word.include?(guess)
     puts "The letter is correct!"
+    unless wrong_letters.empty?
+      "Here are wrong letters: #{wrong_letters}"
+    end
   else
     puts "Sorry, wrong letter"
     wrong_letters << guess
+    puts "Here are wrong letters: #{wrong_letters}"
   end
 end
 
@@ -49,12 +55,35 @@ def init_board(word)
 end
 
 #searching for the player's letter in a word and placing in on the board
-def find_and_place_letter(guess, word, board)
-  letter_index = word.find_index(guess)
-  board[letter_index] = guess
+def find_and_place_letter(guess, word_arr, board)
+  word_arr.each_with_index do |letter, index|
+    if letter == guess
+      board[index] = guess
+    end
+  end
   board
 end
 
+def save_game(word, board, wrong_letters)
+  filename = "saved_game_#{Time.now.strftime("%Y-%m-%d %H-%M")}"
+  File.open(filename, 'w') do |file|
+    file.puts word
+    file.puts board.join(" ")
+    file.puts {wrong_letters.join(" ")}
+  end
+end
+
+def load_game
+  puts "Enter your saved game file name"
+  filename = gets.chomp
+  lines = File.readlines(filename, 'w')
+  output = []
+  lines.each do |line|
+    output << line.chomp
+
+  end
+  output
+end
 
 # p guess_in_word("a", word_arr, wrong_letters, right_letters)
 
@@ -63,10 +92,20 @@ end
 
 show_board(word)
 board = init_board(word)
+# while board.count("_") > 0
 
-guess = player_turn
 
-puts guess_in_word(guess, word, wrong_letters, right_letters)
+  guess = player_turn
 
-board = find_and_place_letter(guess, word_arr, board)
-puts board.join(" ")
+  puts guess_in_word(guess, word, wrong_letters, right_letters)
+
+  board = find_and_place_letter(guess, word_arr, board)
+  puts board.join(" ")
+
+  save_game(word, board, wrong_letters)
+  loaded_game = load_game
+  p loaded_game
+# end
+
+
+puts "Coungrutalations! Your word is #{word}"
